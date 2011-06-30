bbee30e93dd9c598da2495f4ca7b1f895be6ebdc
johnknott <john.knott@gmail.com>
2011-06-30 20:43:59 +0100

    Removed output.

--- lib/pry-padrino.rb
+++ lib/pry-padrino.rb
@@ -3,7 +3,5 @@ begin
   $VERBOSE = nil
   IRB = Pry
   $VERBOSE = false
-  puts "\e[32mUsing Pry instead of IRB.\e[0m"
 rescue LoadError
-  puts "\e[31mFailed to load Pry. Falling back on IRB.\e[0m"  
 end
