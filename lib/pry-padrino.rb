8178b8b2e5cf71d1e1ef8faf6e67ab4703ebd80f
johnknott <john.knott@gmail.com>
2011-06-30 20:36:20 +0100

    Simplified.

--- lib/pry-padrino.rb
+++ lib/pry-padrino.rb
@@ -1,15 +1,9 @@
-module PryPadrino
-  def self.patch_irb
-    begin
-      require 'pry'
-      $VERBOSE = nil
-      Kernel.const_set(:IRB, :Pry)
-      $VERBOSE = false
-      puts "\e[32mUsing Pry instead of IRB.\e[0m"
-    rescue LoadError
-      puts "\e[31mFailed to load Pry. Falling back on IRB.\e[0m"  
-    end
-  end
+begin
+  require 'pry'
+  $VERBOSE = nil
+  IRB = Pry
+  $VERBOSE = false
+  puts "\e[32mUsing Pry instead of IRB.\e[0m"
+rescue LoadError
+  puts "\e[31mFailed to load Pry. Falling back on IRB.\e[0m"  
 end
-
-PryPadrino.patch_irb
